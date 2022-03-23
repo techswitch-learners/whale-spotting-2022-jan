d6ad39a7fdfb12a77bf075cdfb2f8aab83b080fd
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-03-23 15:22:57 +0000

    WS-004: Create sightings controller

---
+++ backend/Controllers/SightingsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WhaleSpotting.Models.Request;
+using WhaleSpotting.Services;
+using WhaleSpotting.Repositories;
+using WhaleSpotting.Helpers;
+using System.Collections.Generic;
+using WhaleSpotting.Models.Database;
+
+namespace WhaleSpotting.Controllers
+{
+    
+}
