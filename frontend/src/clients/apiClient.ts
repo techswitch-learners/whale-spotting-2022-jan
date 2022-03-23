29f84bfac180300650a0df42d8ec4197c0275517
Alison Thomson <alisonmcknight@hotmail.com>
2022-03-23 15:57:09 +0000

    WS-012: Add apiClient with Get Locations fetch

---
+++ frontend/src/clients/apiClient.ts
@@ -0,0 +1,16 @@
+﻿export async function fetchLocations() {
+    const response = await fetch(`https://localhost:5001/locations`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        
+        },
+        // body: JSON.stringify(searchTerm),
+    });
+   
+    if (!response.ok) {
+        throw new Error(await response.json())
+    }
+
+    return await response.json();
+}
--- frontend/src/components/planATripPage/Locations/Locations.tsx
+++ frontend/src/components/planATripPage/Locations/Locations.tsx
@@ -1,10 +1,16 @@
 import react from "react";
 import { getLocations } from "../../../clients/locationClient";
-
 import Select from 'react-select';
+import { fetchLocations } from "../../../clients/apiClient";
+
 
 export function Locations() {
 
+  // const locations = fetchLocations();
+  // locations.forEach(location in locations) {
+  //   value: location.Id, label: location.name;
+  // }
+  
   const options = [
         { value: 'chocolate', label: 'Chocolate' },
         { value: 'strawberry', label: 'Strawberry' },
