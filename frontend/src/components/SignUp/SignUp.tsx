d9c0741b0f6f892e0a8911c17e6ec71749e5dcdc
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-03-23 13:35:23 +0000

    WS-011: Initial commit

---
+++ frontend/src/components/SignUp/SignUp.tsx
@@ -0,0 +1,8 @@
+import React, {FormEvent, useState} from "react";
+import {Link} from "react-router-dom";
+
+type FormStatus = "READY" | "sUBMITTING" | "ERROR" | "FINISHED"
+
+export function SignUpForm() {
+    
+}
