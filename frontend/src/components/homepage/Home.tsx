4ecd6b24aada1622e8cf10ab7a27ab25dd4212ed
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-03-24 15:52:55 +0000

    WS-007: Create Recent Sighting component (html only)

--- frontend/src/components/homepage/Home.tsx
+++ frontend/src/components/homepage/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Home.scss";
+import { RecentSighting } from "./recentSighting/RecentSighting";
 
 export const Home: React.FunctionComponent = () => (
   <>
     <h1>Whale Spotting</h1>
-    <p>Spot whales!</p>
+    <RecentSighting />
   </>
 );
