e610d51d0cf8528ee417dd8b43cfe300b1f8c002
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-03-25 09:39:05 +0000

    WS-007: Create Popular Locations component (html only)

--- frontend/src/components/homepage/Home.tsx
+++ frontend/src/components/homepage/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.scss";
+import { PopularLocations } from "./popularLocations/PopularLocations";
 import { RecentSighting } from "./recentSighting/RecentSighting";
 
 export const Home: React.FunctionComponent = () => (
   <>
     <h1>Whale Spotting</h1>
     <RecentSighting />
+    <PopularLocations />
   </>
 );
---
+++ frontend/src/components/homepage/popularLocations/PopularLocations.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function PopularLocations() {
+  return (
+    <section>
+      <h1>See locations with the most whale sightings:</h1>
+      <ol>
+        <li>Location Name 1: # of sightings</li>
+        <li>Location Name 2: # of sightings</li>
+        <li>Location Name 3: # of sightings</li>
+      </ol>
+    </section>
+  );
+}
