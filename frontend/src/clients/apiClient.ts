fb03485285b15ab338402789d72e89c767fefe29
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-03-25 12:25:13 +0000

    WS-013: Add NewSighting interface and createSighting function to apiClient

--- frontend/src/clients/apiClient.ts
+++ frontend/src/clients/apiClient.ts
@@ -1,3 +1,6 @@
+import internal from "stream";
+import { NumberLiteralType } from "typescript";
+
 export interface Species {
   id: number;
   name: string;
@@ -16,6 +19,19 @@ export interface Location {
   amenities: string[];
 }
 
+export interface NewSighting {
+  date: Date;
+  locationId: number;
+  speciesId: number;
+  description: string;
+  photoUrl: string;
+  userId: number;
+}
+
+function getAuthorizationHeader(username: string, password: string) {
+  return `Basic ${btoa(`${username}:${password}`)}`;
+}
+
 export async function fetchSpecies(): Promise<Array<Species>> {
   const response = await fetch(`https://localhost:5001/species`);
   return await response.json();
@@ -24,4 +40,23 @@ export async function fetchSpecies(): Promise<Array<Species>> {
 export async function fetchLocations(): Promise<Array<Location>> {
   const response = await fetch(`https://localhost:5001/locations`);
   return await response.json();
+}
+
+export async function createSighting(
+  newSighting: NewSighting,
+  username: string,
+  password: string
+) {
+  const response = await fetch(`https://localhost:5001/sightings/create`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: getAuthorizationHeader(username, password),
+    },
+    body: JSON.stringify(newSighting),
+  });
+
+  if (!response.ok) {
+    throw new Error(await response.json());
+  }
 }
