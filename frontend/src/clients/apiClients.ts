3789cc650625a9f51d60602e165446b2ab3b50e5
Deepashri Dali <deepashri@hotmail.com>
2022-03-25 09:41:30 +0000

    WS-012: Add onchange to select location and display weather icons and temprature.

--- frontend/src/clients/apiClients.ts
+++ frontend/src/clients/apiClients.ts
@@ -11,3 +11,38 @@ export const login = async (
     throw new Error(JSON.stringify(await response.json()));
   }
 };
+
+//Api for location
+export type Location = {
+  id: number;
+  name: string;
+  description: string;
+  latitude: number;
+  longitude: number;
+  amenities: Array<string>;
+};
+export async function fetchLocations(): Promise<Array<Location>> {
+  const response = await fetch(`https://localhost:5001/locations`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(await response.json());
+  }
+  return await response.json();
+}
+export async function fetchLocationById(): Promise<Location> {
+  const response = await fetch(`https://localhost:5001/locations`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error(await response.json());
+  }
+  return await response.json();
+}
