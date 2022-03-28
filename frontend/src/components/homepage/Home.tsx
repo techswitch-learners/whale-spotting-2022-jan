64dcea1dc8259439fc3ea26f6adc441e30a5ae44
Sam James <samjojames@gmail.com>
2022-03-28 16:37:03 +0100

    WS-007: Add some very basic styling to test visuals

--- frontend/src/components/homepage/Home.tsx
+++ frontend/src/components/homepage/Home.tsx
@@ -3,10 +3,12 @@ import "./Home.scss";
 import { PopularLocations } from "./popularLocations/PopularLocations";
 import { RecentSighting } from "./recentSighting/RecentSighting";
 
-export const Home: React.FunctionComponent = () => (
-  <>
-    <h1>Whale Spotting</h1>
-    <RecentSighting />
-    <PopularLocations />
-  </>
-);
+export const Home: React.FunctionComponent = () => {
+  return (
+    <div className="homepage">
+      <h1>Whale Spotting</h1>
+      <RecentSighting />
+      <PopularLocations />
+    </div>
+  );
+};
--- frontend/src/components/homepage/recentSighting/RecentSighting.tsx
+++ frontend/src/components/homepage/recentSighting/RecentSighting.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { GetMostRecentSighting, Sighting } from "../../../clients/apiClients";
+import "./RecentSighting.scss";
 
 export function RecentSighting() {
   const [recentSighting, setRecentSighting] = useState<Sighting>();
@@ -8,16 +9,20 @@ export function RecentSighting() {
   }, []);
 
   return (
-    <section>
+    <section className="recent-sighting">
       <h1>Most Recent Whale Sighting 🐳</h1>
       <p>
         A {recentSighting?.species} Spotted on {recentSighting?.date} at{" "}
         {recentSighting?.location}
       </p>
-      <figure>
-        <img src={recentSighting?.photoUrl} width="250px" />
-        <figcaption>{recentSighting?.description}</figcaption>
-      </figure>
+      <div className="img-container">
+        <img
+          src={recentSighting?.photoUrl}
+          width="250px"
+          className="rounded-pill"
+        />
+      </div>
+      <p className="p-5">{recentSighting?.description}</p>
     </section>
   );
 }
