c7db5eb7ba8655a2b623f1ec0bd27fa7690b7610
Ija Saporenkova <Ija.Saporenkova@softwire.com>
2022-03-30 15:51:41 +0100

    WS-024: Add right link to the menu

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -2,43 +2,52 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { LoginContext } from "../login/LoginManager";
 import "./Navbar.scss";
+import { slide as Menu } from "react-burger-menu";
 
 export const Navbar: React.FunctionComponent = () => {
   const loginContext = useContext(LoginContext);
 
   return (
-    <nav role="navigation" aria-label="main navigation">
+    // <nav role="navigation" aria-label="main navigation">
+    //   <Link to="/">
+    //     <img src="/logo.png" alt="Whale Spotting logo" />
+    //   </Link>
+    //   <div>
+    //     {!loginContext.isLoggedIn ? (
+    //       <div>
+    //         <Link className="button is-primary" to="/login">
+    //           Login
+    //         </Link>
+    //       </div>
+    //     ) : (
+    //       <Link
+    //         to="/"
+    //         className="button is-primary"
+    //         onClick={loginContext.logOut}
+    //       >
+    //         Logout
+    //       </Link>
+    //     )}
+    //   </div>
+    // </nav>
+    <div className="navbar__menu">
       <Link to="/">
         <img src="/logo.png" alt="Whale Spotting logo" />
       </Link>
-      <Link to="/sign-up">
-        <a>Sign Up</a>
-      </Link>
-      <span> </span>
-      <Link to="/sightings/create">
-        <a>Report Sighting</a>
-      </Link>
-      <span> </span>
-      <Link to="/sightings">
-        <a>All Sightings</a>
-      </Link>
-      <div>
-        {!loginContext.isLoggedIn ? (
-          <div>
-            <Link className="button is-primary" to="/login">
-              Login
-            </Link>
-          </div>
-        ) : (
-          <Link
-            to="/"
-            className="button is-primary"
-            onClick={loginContext.logOut}
-          >
-            Logout
-          </Link>
-        )}
-      </div>
-    </nav>
+      <Menu right>
+        <a id="home" className="menu-item" href="/">
+          Home
+        </a>
+        <a id="sightings" className="menu-item" href="/sightings">
+          Sightings
+        </a>
+        <a id="ReportSighting" className="menu-item" href="/sightings/create">
+          Report a sighting
+        </a>
+        <a id="Login" className="menu-item" href="/Login">
+          Login
+        </a>
+      </Menu>
+    </div>
   );
 };
