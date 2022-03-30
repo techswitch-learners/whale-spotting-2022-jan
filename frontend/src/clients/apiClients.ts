0db498186c1954ff5b939845b5b8492ad84e5d5c
Tim Leach <timleach635@gmail.com>
2022-03-30 18:59:51 +0100

    WS-000: Remove extra models from apiClient.ts

--- frontend/src/clients/apiClients.ts
+++ frontend/src/clients/apiClients.ts
@@ -9,25 +9,6 @@ export interface NewUser {
   email: string;
   password: string;
 }
-export interface Species {
-  name: string;
-  latinName: string;
-}
-
-export interface Sighting {
-  id: number;
-  date: Date;
-  location: Location;
-  description: string;
-  species: Species;
-  photoUrl: string;
-  user: User;
-}
-
-export interface Species {
-  name: string;
-  latinName: string;
-}
 
 export interface Location {
   id: number;
