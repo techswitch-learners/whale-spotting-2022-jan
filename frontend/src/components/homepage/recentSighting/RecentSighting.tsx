c3daa36443d2a76d5b8025da45a3bd7d121be6e0
Sam James <samjojames@gmail.com>
2022-03-31 12:34:29 +0100

    WS-024: Seperate info into boxes and add button

--- frontend/src/components/homepage/recentSighting/RecentSighting.tsx
+++ frontend/src/components/homepage/recentSighting/RecentSighting.tsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 import { getMostRecentSighting, Sighting } from "../../../clients/apiClients";
 import "./RecentSighting.scss";
-import ReactCardFlip from "react-card-flip";
 import { Link } from "react-router-dom";
-// import  {Polaroid} from "polaroid-image";
+import { Button } from "react-bootstrap";
 
 export function RecentSighting() {
   const [recentSighting, setRecentSighting] = useState<Sighting>();
-  const [isFlipped, setIsFlipped] = useState(false);
 
   useEffect(() => {
     getMostRecentSighting().then(setRecentSighting);
@@ -16,10 +14,6 @@ export function RecentSighting() {
   if (recentSighting == undefined) {
     return <div> Loading ... </div>;
   } else {
-    const handleClick = () => {
-      setIsFlipped(!isFlipped);
-    };
-
     const today = new Date();
     const date2 = new Date(recentSighting.date);
 
@@ -38,21 +32,28 @@ export function RecentSighting() {
       <section className="recent-sighting">
         <section>
           <div className="recent-sighting__img-front">
-            <img
-              src={recentSighting.photoUrl}
-              width="500px"
-              onClick={handleClick}
-            ></img>
+            <img src={recentSighting.photoUrl}></img>
           </div>
         </section>
         <h1 className="recent-sighting__title">Most Recent Whale Sighting</h1>
-        <section className="recent-sighting__text">
-          <p>
-            {article} {recentSighting?.species.name} (Latin name:{" "}
-            {recentSighting.species.latinName}) spotted {diffDays} days ago at{" "}
-            {recentSighting?.location.name} by {recentSighting.user.username}.
-          </p>
-          <p>Description of the sighting: {recentSighting?.description}</p>
+        <section className="recent-sighting_text-container">
+          <section className="recent-sighting__text recent-sighting__left">
+            <p>
+              {article} {recentSighting?.species.name} (Latin name:{" "}
+              {recentSighting.species.latinName}) spotted {diffDays} days ago at{" "}
+              {recentSighting?.location.name} by {recentSighting.user.username}.
+            </p>
+            <p>Description of the sighting: {recentSighting?.description}</p>
+          </section>
+          <section className="recent-sighting__text recent-sighting__right">
+            <h2 className="">Report your own sighting!</h2>
+            <Button
+              variant="light"
+              className="recent-sighting__post-sighting-button"
+            >
+              +
+            </Button>{" "}
+          </section>
         </section>
       </section>
     );
