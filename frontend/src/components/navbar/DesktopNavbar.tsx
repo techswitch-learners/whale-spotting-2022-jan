a0afbeb0a9e3a63a53331033ab37712fd4e68eff
Ling Balmforth <Ling.Balmforth@softwire.com>
2022-04-01 17:10:42 +0100

    WS-028: WIP Create sightings sub-menu

--- frontend/src/components/navbar/DesktopNavbar.tsx
+++ frontend/src/components/navbar/DesktopNavbar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { LoginContext } from "../login/LoginManager";
 import "./DesktopNavbar.scss";
@@ -7,57 +7,71 @@ export const DesktopNavbar: React.FunctionComponent = () => {
   const loginContext = useContext(LoginContext);
 
   return (
-    <div className="navbar__menu navbar__horizontal">
-      <li>
-        <Link to="/">
-          <img src="/logo.png" alt="Whale Spotting logo" />
-        </Link>{" "}
-      </li>
-
-      <div className="navbar__horizontal_content">
-        <div className="navbar__horizontal_content_left">
-          {" "}
-          <span></span>
-          {loginContext.isLoggedIn ? (
-            <span>
-              {"Whale-come " + loginContext.username + "!"}
+    <div className="desktop-navbar">
+      <Link to="/">
+        <img
+          className="desktop__logo"
+          src="/logo.png"
+          alt="Whale Spotting logo"
+        />
+      </Link>{" "}
+      <div className="desktop__login">
+        {" "}
+        {loginContext.isLoggedIn ? (
+          "Whale-come " + loginContext.username + "!"
+        ) : (
+          <Link to="/login"> Login </Link>
+        )}
+      </div>
+      <ul className="desktop__menubar">
+        <li className="desktop__menuitem">
+          <Link to="/"> Home </Link>
+        </li>
+        <li>
+          <span>Sighting</span>
+          <div>
+            <li className="desktop__menuitem">
+              <Link to="/sightings">All sightings </Link>
+            </li>
+            <li className="desktop__menuitem">
+              <Link to="/sightings/create">Report a sighting </Link>
+            </li>
+          </div>
+        </li>
+        <li className="desktop__menuitem">
+          <Link to="/plantrip"> Plan a Trip! </Link>
+        </li>
+        {!loginContext.isLoggedIn ? (
+          <li className="desktop__menuitem">
+            <Link to="/sign-up"> Sign Up </Link>
+          </li>
+        ) : (
+          <></>
+        )}
+        <div>
+          {!loginContext.isLoggedIn ? (
+            <div>
+              <li className="desktop__menuitem">
+                <Link className="button is-primary" to="/login">
+                  Login
+                </Link>
+              </li>
+            </div>
+          ) : (
+            <li className="desktop__menuitem">
               <Link
                 to="/"
                 className="button is-primary"
-                onClick={loginContext.logOut}
+                onClick={() => {
+                  loginContext.logOut();
+                }}
               >
                 Logout
               </Link>
-            </span>
-          ) : (
-            <Link to="/login"> Login </Link>
+            </li>
           )}
         </div>
-
-        <div className="navbar__horizontal_content_right">
-          <ul>
-            <li>
-              <Link to="/"> Home </Link>
-            </li>
-            <li>
-              <Link to="/sightings"> Sightings </Link>
-            </li>
-            <li>
-              <Link to="/sightings/create">Report a sighting </Link>
-            </li>
-            <li>
-              <Link to="/plantrip"> Plan a Trip! </Link>
-            </li>
-            {!loginContext.isLoggedIn ? (
-              <li>
-                <Link to="/sign-up"> Sign Up </Link>
-              </li>
-            ) : (
-              <></>
-            )}
-          </ul>
-        </div>
-      </div>
+      </ul>
     </div>
   );
 };
