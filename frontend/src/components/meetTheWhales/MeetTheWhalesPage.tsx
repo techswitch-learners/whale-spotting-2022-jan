2e6976647fc89c431d08aed52d3c4fb2fc609025
Ling Balmforth <Ling.Balmforth@softwire.com>
2022-04-06 14:11:25 +0100

    WS-037: Add toastify delete confirmation

--- frontend/src/components/meetTheWhales/MeetTheWhalesPage.tsx
+++ frontend/src/components/meetTheWhales/MeetTheWhalesPage.tsx
@@ -3,6 +3,14 @@ import "./MeetTheWhalesPage.scss";
 import { deleteSpecies, fetchSpecies, Species } from "../../clients/apiClients";
 import { LoginContext } from "../../components/login/LoginManager";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
+
+export const notifyOfSuccessfulDelete = () => {
+  toast.success("Species deleted!");
+};
 
 export function MeetTheWhalesPage(): JSX.Element {
   const [species, setSpecies] = useState<Array<Species>>([]);
@@ -15,7 +23,7 @@ export function MeetTheWhalesPage(): JSX.Element {
   const onDeleteSpecies = (speciesId: number) => {
     if (speciesId) {
       deleteSpecies(speciesId, username, password).then(() =>
-        fetchSpecies().then(setSpecies)
+        fetchSpecies().then(setSpecies).then(notifyOfSuccessfulDelete)
       );
     }
   };
