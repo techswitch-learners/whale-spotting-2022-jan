ac4c3b5e570a560d9877d7e83fa653761995f98e
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-04-07 12:34:31 +0100

    WS-034: Create separate conditions for loading sightings list and empty sightings list

--- frontend/src/pages/Sightings/SightingListPage.tsx
+++ frontend/src/pages/Sightings/SightingListPage.tsx
@@ -12,15 +12,18 @@ import {
 } from "../../clients/apiClients";
 import { LoginContext } from "../../components/login/LoginManager";
 import { SightingList } from "../../components/SightingListPage/SightingList/SightingList";
+import { Link } from "react-router-dom";
 
 export function SightingListPage(): JSX.Element {
   const [sightings, setSightings] = useState<Array<Sighting>>([]);
   const [combinedSightingList, setCombinedSightingList] = useState<
     Array<Sighting | ExternalSighting>
   >([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const { username, password } = useContext(LoginContext);
 
   useEffect(() => {
+    setLoading(true);
     Promise.all([getAllSightings(), getExternalSightings()]).then(
       ([sightings, externalSightings]) => {
         const combinedSightings: Array<Sighting | ExternalSighting> = [];
@@ -29,12 +32,23 @@ export function SightingListPage(): JSX.Element {
             .concat(sightings, externalSightings)
             .sort((a, b) => +new Date(b.date) - +new Date(a.date))
         );
+        setLoading(false);
       }
     );
   }, []);
 
-  if (combinedSightingList.length == 0) {
-    return <div>loading...</div>;
+  if (loading) {
+    return <h1 className="sighting__list__loading">Loading...</h1>;
+  }
+
+  if (combinedSightingList.length === 0) {
+    return (
+      <h1 className="sighting__list__loading">
+        There are no whale sightings... yet!
+        <Link to="/sightings/create"> Click here </Link>
+        to report the first sighting.
+      </h1>
+    );
   }
 
   return (
