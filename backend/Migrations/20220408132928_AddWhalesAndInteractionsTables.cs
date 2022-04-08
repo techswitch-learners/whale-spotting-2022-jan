12f74b76f8f6997ee82e446e08d544893024a48d
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-04-08 14:33:32 +0100

    WS-032: Recreate migrations after merge

---
+++ backend/Migrations/20220408132928_AddWhalesAndInteractionsTables.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WhaleSpotting.Migrations
+{
+    public partial class AddWhalesAndInteractionsTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Whales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    PhotoUrl = table.Column<string>(type: "text", nullable: true),
+                    SpeciesId = table.Column<int>(type: "integer", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Whales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Whales_Species_SpeciesId",
+                        column: x => x.SpeciesId,
+                        principalTable: "Species",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interaction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    WhaleId = table.Column<int>(type: "integer", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Interaction_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Interaction_Whales_WhaleId",
+                        column: x => x.WhaleId,
+                        principalTable: "Whales",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interaction_UserId",
+                table: "Interaction",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Interaction_WhaleId",
+                table: "Interaction",
+                column: "WhaleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Whales_SpeciesId",
+                table: "Whales",
+                column: "SpeciesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interaction");
+
+            migrationBuilder.DropTable(
+                name: "Whales");
+        }
+    }
+}
