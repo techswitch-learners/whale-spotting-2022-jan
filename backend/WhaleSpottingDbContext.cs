517751a74f9274f51bc9df34d246c65e30cd0436
Tim Leach <timleach635@gmail.com>
2022-06-27 11:35:30 +0100

    Revert database to initial

--- backend/WhaleSpottingDbContext.cs
+++ backend/WhaleSpottingDbContext.cs
@@ -8,7 +8,6 @@ namespace WhaleSpotting
     public class WhaleSpottingDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-        public DbSet<Location> Locations{ get; set; }
 
         protected override void OnConfiguring(
             DbContextOptionsBuilder optionsBuilder
